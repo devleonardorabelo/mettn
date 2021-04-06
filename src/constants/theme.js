f583a3d175666e2c7ce60a6e037cc723b5c5d29c
Leonardo Rabelo <devleonardorabelo@gmail.com>
2021-04-05 21:34:21 -0300

    feat: added theme colors

---
+++ src/constants/theme.js
@@ -0,0 +1,9 @@
+export const COLORS = {
+  primaryPurple: '#C93DFA',
+  secondaryPurple: '#2F2136',
+  darkPurple: '#1F1524',
+  gray: '#969696',
+  red: '#FA3D3D',
+  green: '#08C732',
+  white: '#08C732',
+};
