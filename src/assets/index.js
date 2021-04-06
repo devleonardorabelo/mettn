7f2c29a0289fde18ac7c0fcca507ae1167d9bbe6
Leonardo Rabelo <devleonardorabelo@gmail.com>
2021-04-05 21:34:05 -0300

    feat: new icons and people image

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+export const IMAGES = {
+  logo: require('./images/icons/logo.png'),
+  back: require('./images/icons/back.png'),
+  chat: require('./images/icons/chat.png'),
+  deny: require('./images/icons/deny.png'),
+  heart: require('./images/icons/heart.png'),
+  star: require('./images/icons/star.png'),
+};
